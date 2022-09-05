71d7fa7cfea3e43f0b5fc0a6e778670bca46c306
Shaiane Boesing <shaianeboesingrb@gmail.com>
2022-09-05 11:30:56 -0300

    :sparkles: Feat: testando hashes em Ruby

---
+++ hash_test.rb
@@ -0,0 +1,12 @@
+my_hash = Hash.new
+for i in 1..3
+    print ("#{i}ª chave: ")
+    chave = gets.chomp
+    print ("#{i}º valor: ")
+    valor = gets.chomp
+    my_hash[chave] = valor
+end
+
+my_hash.each do |key, value|
+    puts "Uma das chaves é #{key} e o seu valor é: #{value}"
+end
